3b6edef1a611cb4d566623f0e14882153d42a068
DevSuelen <117091949+DevSuelen@users.noreply.github.com>
2023-01-22 08:08:40 -0300

    add readme

--- script.js
+++ script.js
@@ -5,24 +5,22 @@ button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
-
-  const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
 
-  if(dayExists){
+  if (dayExists) {
     alert("Dia já incluso 🚨")
     return
   }
-  alert('Adicionado com sucesso ✅')
+  alert("Adicionado com sucesso ✅")
   nlwSetup.addDay(today)
-
 }
 
 function save() {
-  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data) )
-
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
+clear
